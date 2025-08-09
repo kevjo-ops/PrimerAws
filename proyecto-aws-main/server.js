8739efa18d2e18d669e345a53dcebbc338766b73
kevjo-ops <kevinjpilcohuilca@example.com>
2025-08-08 21:11:21 -0500

    ProyectoAWS

---
+++ proyecto-aws-main/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('¡Hola este es mi primer despliege web con aws y node.js!');
+});
+
+const PORT = process.env.PORT || 80;
+app.listen(PORT, '0.0.0.0', () => console.log(`Servidor en puerto ${PORT}`));
